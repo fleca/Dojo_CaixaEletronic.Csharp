dea3e92d50fe84b831ff0fb3789d2f2421fa8dbd
fleca <36854603+fleca@users.noreply.github.com>
2018-03-24 22:26:25 -0300

    Add files via upload

---
+++ GravadorTexto.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Util.ArquivoTexto
+{
+    public class GravadorArquivo
+    {
+        public GravadorArquivo()
+        {
+
+        }
+
+        public bool EscreverArquivo(string nomeArquivo, string linha)
+        {
+            try
+            {
+                using (var writer = new StreamWriter(nomeArquivo, true /*adicionar novas coisas, false reescreveria o arquivo*/, Encoding.UTF8))
+                {
+                    writer.WriteLine(linha);
+                }
+                return true;
+            }
+            catch (Exception ex)
+            {
+                //TODO: tratar exception
+                return false;
+            }
+        }
+    }
+}
